c9e14cf3ab7ac32a0f48c94ad83c24707e72425e
0yech <Nelson.rey02@gmail.com>
2025-01-28 17:23:09 +0100

    started export.c

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,9 +6,42 @@
 /*   By: cheyo <cheyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:28:50 by cheyo             #+#    #+#             */
-/*   Updated: 2025/01/28 15:34:32 by cheyo            ###   ########.fr       */
+/*   Updated: 2025/01/28 17:22:43 by cheyo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int		ft_arraysize(char **p)
+{
+	int i;
+
+	i = 0;
+	while (p[i])
+		i++;
+	return (i);
+}
+
+void	ft_export(char *value)
+{
+	char *var;
+	char **check;
+
+	check = ft_split(value, '=');
+	if (check == NULL)
+		return ;
+	if (ft_arraysize(check) == 1)
+	{
+		var = getenv(value);
+		if (var == NULL)
+			return ;
+	}
+	else if (ft_arraysize(check) == 2)
+	{
+		var = getenv(check[0]);
+	}	
+	else
+	{
+		free_array(check);
+	}
+}

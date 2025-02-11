cf12231a67d59cd3a65574f1e733a7fcf6ac287b
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-02-11 17:45:56 +0100

    fill_envcpy() now also fills prev pointers

--- src/environ/environ.c
+++ src/environ/environ.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 15:50:52 by estettle          #+#    #+#             */
-/*   Updated: 2025/02/11 14:29:19 by estettle         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:44:49 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,24 +31,25 @@ t_env	*fill_envcpy(char **envp)
 	t_env	*head;
 	t_env	*current;
 	t_env	*new_node;
-	int		i;
 
-	i = 0;
 	head = NULL;
 	current = NULL;
-	while (envp[i])
+	while (*envp)
 	{
 		new_node = ft_calloc(1, sizeof(t_env));
 		if (!new_node)
 			return (env_clear(&head), NULL);
-		if (!env_fill_node(envp[i], new_node))
+		if (!env_fill_node(*envp, new_node))
 			return (env_delone(new_node), env_clear(&head), NULL);
 		if (!head)
 			head = new_node;
 		else
-			current->next = new_node; // prev is never set, we have a unidirectional list so far, easy fix
+		{
+			current->next = new_node;
+			new_node->prev = current;
+		}
 		current = new_node;
-		i++;
+		envp++;
 	}
 	return (head);
 }

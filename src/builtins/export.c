7990ba25dc468a434cdf8bde1591f0663dc6d7c2
Aisling <aisling.fontaine@protonmail.com>
2025-02-12 14:57:20 +0100

    fix export leak

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,14 +6,14 @@
 /*   By: cheyo <cheyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:28:50 by cheyo             #+#    #+#             */
-/*   Updated: 2025/02/12 13:46:39 by estettle         ###   ########.fr       */
+/*   Updated: 2025/02/12 14:56:31 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /**
- * @return 0 if everything went well, 1 if an error occured.
+ * @return 0 if everything went well, 1 if an error occurred.
  */
 int	ft_export(char *str)
 {
@@ -28,9 +28,11 @@ int	ft_export(char *str)
 	{
 		slices = ft_split(str, '=');
 		if (!slices[1])
-			return (printf("[!] - Invalid identifier!\n"), free(slices[0]), free(slices[1]), 1);
+			return (printf("[!] - Invalid identifier!\n"), free_array(slices), 1);
 		if (env_set(slices[0], slices[1]) == 2)
-			return (free(slices[2]), 1);
+			return (free_array(slices), 1);
+		free(slices[2]);
+		free(slices);
 	}
 	else
 		return (printf("[!] - Export without = isn't supported yet!\n"), 1);
--- src/environ/env_set_list.c
+++ src/environ/env_set_list.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 16:49:07 by nrey              #+#    #+#             */
-/*   Updated: 2025/02/11 17:31:29 by estettle         ###   ########.fr       */
+/*   Updated: 2025/02/12 14:53:14 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,6 +75,8 @@ int	env_set(char *key, char *value)
 	node = get_key(key);
 	if (node)
 	{
+		if (node->value)
+			free(node->value);
         node->value = ft_strdup(value);
 		if (!node->value)
 			return (2);

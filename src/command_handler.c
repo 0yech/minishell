353bd222d11333645b3486157e92bf3b9326fbbb
nrey <Nelson.rey02@gmail.com>
2025-02-24 11:03:24 +0100

    changed len numbers to ft_strlen on command_handler

--- src/command_handler.c
+++ src/command_handler.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 15:34:36 by cheyo             #+#    #+#             */
-/*   Updated: 2025/02/19 11:46:47 by estettle         ###   ########.fr       */
+/*   Updated: 2025/02/24 11:03:02 by nrey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@ void	command_handler(t_token **token_list)
 {
 	// TODO Need to take into account int return values of builtins
 	// printf("%s\n", (*token_list)->value);
-	if (!ft_strncmp((*token_list)->value, "exit", 4))
+	if (!ft_strncmp((*token_list)->value, "exit", ft_strlen((*token_list)->value)))
 		ft_exit(token_list);
-	if (!ft_strncmp((*token_list)->value, "pwd", 3))
+	if (!ft_strncmp((*token_list)->value, "pwd", ft_strlen((*token_list)->value)))
 		ft_pwd();
-	if (!ft_strncmp((*token_list)->value, "cd", 2))
+	if (!ft_strncmp((*token_list)->value, "cd", ft_strlen((*token_list)->value)))
 		ft_cd(token_list);
-	if (!ft_strncmp((*token_list)->value, "env", 3))
+	if (!ft_strncmp((*token_list)->value, "env", ft_strlen((*token_list)->value)))
 		ft_env();
-	if (!ft_strncmp((*token_list)->value, "export", 6))
+	if (!ft_strncmp((*token_list)->value, "export", ft_strlen((*token_list)->value)))
 	{
 		if ((*token_list)->next)
 			ft_export((*token_list)->next->value);
 		else
 			ft_export(NULL);
 	}
-	if (!ft_strncmp((*token_list)->value, "unset", 5))
+	if (!ft_strncmp((*token_list)->value, "unset", ft_strlen((*token_list)->value)))
 	{
 		if ((*token_list)->next)
 			ft_unset((*token_list)->next->value);

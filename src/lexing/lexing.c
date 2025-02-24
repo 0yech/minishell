0986168369c5b08b19108fecd74e4a3110a73d26
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-02-24 10:35:38 +0100

    fix condition for calling quotes_handler

--- src/lexing/lexing.c
+++ src/lexing/lexing.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:39:44 by cheyo             #+#    #+#             */
-/*   Updated: 2025/02/23 16:51:19 by estettle         ###   ########.fr       */
+/*   Updated: 2025/02/24 10:35:09 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,24 +30,23 @@ static int	get_next_token(t_token **token_list, char *input)
 		return (-1);
 	while (input && input[i] && ft_isprint(input[i]) && input[i] != ' ')
 	{
-		/*
-		if (input[i] == '\'' || input[i] == '"')
+		if ((input[i] == '\'' || input[i] == '"')
+			&& (i == 0 || (!ft_isprint(input[i - 1]) || input[i - 1] == ' ')))
 		{
 			i = quotes_handler(input + i);
+			new->value = ft_substr(input, 0, i);
 			// new->value = quotes_clean(ft_substr(input, 0, i));
-			new->value = quotes_clean(ft_substr(input, 0, i));
 			if (!(new->value))
 				return (free(new), -1);
+			token_add_back(token_list, new);
 			return (i);
 		}
-		*/
 		i++;
 	}
 	new->value = ft_substr(input, 0, i);
 	if (!new->value)
 		return (free(new), -1);
-	token_add_back(token_list, new);
-	return (i);
+	return (token_add_back(token_list, new), i);
 }
 
 static t_token	**create_tokens(char *input)

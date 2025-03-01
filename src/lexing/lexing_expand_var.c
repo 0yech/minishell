7b521524bd075cfa002a5fb02c10c2b164ea5e95
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-01 13:16:58 +0100

    get_variable not so deluxe anymore

--- src/lexing/lexing_expand_var.c
+++ src/lexing/lexing_expand_var.c
@@ -6,14 +6,16 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 17:31:57 by estettle          #+#    #+#             */
-/*   Updated: 2025/03/01 12:07:08 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/01 13:15:31 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// TODO: Refactor this function to not use a malloc, we don't really need one
 char	*get_variable(char *str)
 {
+	size_t	i;
 	char	*key;
 	char	*raw_var;
 	char	*var;
@@ -21,13 +23,17 @@ char	*get_variable(char *str)
 	key = ft_calloc(ft_strlen(str), sizeof(char));
 	if (!key)
 		return (NULL);
-	ft_strlcpy(key, str, ft_strchr(str, ' ') - str);
+	i = 0;
+	while (ft_isalnum(str[i]))
+		i++;
+	ft_strlcpy(key, str, i + 1);
 	raw_var = get_key(key)->value;
 	if (!raw_var)
 		return (NULL);
 	var = ft_strchr(raw_var, '=');
 	if (!var)
 		return (NULL);
+	free(key);
 	return (var);
 }
 
@@ -43,8 +49,10 @@ int	full_token_size(char *raw_token)
 	{
 		if (raw_token[i] == '$')
 		{
+			i++;
 			var = get_variable(raw_token + i);
-			size += ft_strlen(var);
+			if (var)
+				size += ft_strlen(var);
 			while (ft_isprint(raw_token[i]) && raw_token[i] != ' ')
 				i++;
 		}
@@ -56,36 +64,20 @@ int	full_token_size(char *raw_token)
 
 char	*var_expand(char *token)
 {
-	int		i;
+	size_t		i;
+	size_t		j;
 	char	*expanded_token;
-	char	*variable;
 
-	if (!token)
-		return (NULL);
-	expanded_token = malloc(full_token_size(token) + 1);
+	expanded_token = ft_calloc(full_token_size(token), sizeof(char));
 	if (!expanded_token)
-		return (free(token), NULL);
+		return (NULL);
 	i = 0;
+	j = 0;
 	while (token[i])
 	{
-		if (token[i] == '\'')
-		{
-			i++;
-			while (token[i] != '\'')
-				i++;
-		}
-		if (token[i] == '$')
-		{
-			variable = get_variable(token + i);
-			if (variable == NULL)
-			{
-				i++;
-				continue ;
-			}
-			ft_strlcpy(token + i, variable, ft_strlen(variable));
-			free(variable);
-		}
+		expanded_token[j] = token[i];
 		i++;
+		j++;
 	}
 	free(token);
 	return (expanded_token);

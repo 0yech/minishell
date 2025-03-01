f23707db9eebf1c36023c0751e5609f5958da105
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-01 11:44:36 +0100

    more progress on variable expansion

--- src/lexing/lexing.c
+++ src/lexing/lexing.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 16:39:44 by cheyo             #+#    #+#             */
-/*   Updated: 2025/02/27 17:07:43 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/01 11:01:19 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ static int	get_next_token(t_token **token_list, char *input)
 		{
 			i = quotes_handler(input);
 			new->value = quotes_clean(var_expand(ft_substr(input, 0, i)));
+			// new->value = quotes_clean(ft_substr(input, 0, i));
 			if (!(new->value))
 				return (free(new), -1);
 			return (token_add_back(token_list, new), i);
--- src/lexing/lexing_expand_var.c
+++ src/lexing/lexing_expand_var.c
@@ -6,7 +6,7 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 17:31:57 by estettle          #+#    #+#             */
-/*   Updated: 2025/02/28 16:25:37 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/01 11:41:14 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,31 @@
 
 int	full_token_size(char *raw_token)
 {
-	(void)raw_token;
-	return (0);
+	int	i;
+	int	size;
+
+	i = 0;
+	size = 0;
+	while (raw_token[i])
+	{
+		size++;
+		i++;
+	}
+	return (size);
 }
 
 char	*get_variable(char *str)
 {
-	(void)str;
-	return (0);
+	char	*raw_var;
+	char	*var;
+
+	raw_var = get_key(str)->value;
+	if (!raw_var)
+		return (NULL);
+	var = ft_strchr(raw_var, '=');
+	if (!var)
+		return (NULL);
+	return (var);
 }
 
 char	*var_expand(char *token)
@@ -32,18 +49,26 @@ char	*var_expand(char *token)
 
 	if (!token)
 		return (NULL);
-	expanded_token = malloc(ft_strlen(token) + full_token_size(token) + 1);
+	expanded_token = malloc(full_token_size(token) + 1);
 	if (!expanded_token)
 		return (free(token), NULL);
 	i = 0;
 	while (token[i])
 	{
-		if (token[i] == '\'' && ++i)
+		if (token[i] == '\'')
+		{
+			i++;
 			while (token[i] != '\'')
 				i++;
+		}
 		if (token[i] == '$')
 		{
 			variable = get_variable(token + i);
+			if (variable == NULL)
+			{
+				i++;
+				continue ;
+			}
 			ft_strlcpy(token + i, variable, ft_strlen(variable));
 			free(variable);
 		}

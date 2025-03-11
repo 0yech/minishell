d52a00c4126940182e290475d10fdf27c214d1fe
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-11 14:48:19 +0100

    perror export

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: cheyo <cheyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:28:50 by cheyo             #+#    #+#             */
-/*   Updated: 2025/02/12 14:56:31 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/11 14:41:54 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,23 @@ int	ft_export(char *str)
 
 	if (!str)
 	{
-		ft_env(); // Need to write the exported values with export beforehand
+		// TODO Need to write the exported values with export beforehand
+		ft_env();
 		return (0);
 	}
 	if (ft_strchr(str, '='))
 	{
 		slices = ft_split(str, '=');
+		if (!slices)
+			return (perror("minishell (ft_export) - ft_split"), -1);
 		if (!slices[1])
-			return (printf("[!] - Invalid identifier!\n"), free_array(slices), 1);
+			return (printf("[!] - Invalid identifier!\n"), free_array(slices), -1);
 		if (env_set(slices[0], slices[1]) == 2)
 			return (free_array(slices), 1);
 		free(slices[2]);
 		free(slices);
 	}
 	else
-		return (printf("[!] - Export without = isn't supported yet!\n"), 1);
+		return (printf("[!] - Export without = isn't supported yet!\n"), -1);
 	return (0);
 }

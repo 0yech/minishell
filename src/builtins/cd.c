053eeb43b607bafa6c3cb7f71cb901953baee9a2
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-12 17:39:57 +0100

    spring cleaning of cd builtin

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 14:47:25 by estettle          #+#    #+#             */
-/*   Updated: 2025/03/12 13:06:00 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/12 17:39:15 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,46 +15,65 @@
 static int	cd_update_env(char *old_pwd, char *new_pwd)
 {
 	if (env_set("OLDPWD", old_pwd) == 2)
+	{
+		printf("minishell (cd_update_env) - env_set: "
+		 "Failed to change environment!\n");
 		return (1);
+	}
 	if (env_set("PWD", new_pwd) == 2)
+	{
+		printf("minishell (cd_update_env) - env_set: "
+		 "Failed to change environment!\n");
 		return (1);
+	}
 	return (0);
 }
 
 static int	cd_home(void)
 {
+	t_env	*home;
 	char	*old_cwd;
-	char	*new_cwd;
 
+	home = get_key("HOME");
+	if (!home)
+		return (printf("minishell (cd_home) - get_key: HOME is not set!\n"), 1);
 	old_cwd = getcwd(NULL, 0);
-	if (chdir(get_key("HOME")->value) == 0)
+	if (!old_cwd)
+		return (perror("minishell (cd_home) - getcwd"), 1);
+	if (chdir(home->value) == 0)
 	{
-		new_cwd = getcwd(NULL, 0);
-		if (cd_update_env(old_cwd, new_cwd) == 0)
-			return (0);
-		return (free(old_cwd), free(new_cwd), 1);
+		if (cd_update_env(old_cwd, home->value) == 0)
+			return (free(old_cwd), 0);
+		return (free(old_cwd), 1);
 	}
-	return (free(old_cwd), 1);
+	return (perror("minishell (cd_home) - chdir"), free(old_cwd), 1);
 }
 
 static int	cd_swap_old(void)
 {
-	char	*pwd;
-	char	*old_pwd;
+	t_env	*pwd;
+	t_env	*oldpwd;
+	char	*tmp;
 
-	if (!get_key("OLDPWD"))
-		return (printf("[!] - No OLDPWD\n"), 1);
-	old_pwd = ft_strdup(get_key("OLDPWD")->value);
-	if (get_key("OLDPWD") && chdir(get_key("OLDPWD")->value) == 0)
+	oldpwd = get_key("OLDPWD");
+	if (!oldpwd)
+		return (printf("minishell (cd_swap_old) - "
+				  "get_key: OLDPWD is not set!\n"), 1);
+	tmp = ft_strdup(oldpwd->value);
+	if (!tmp)
+		return (perror("minishell (cd_swap_old) - ft_strdup"), 1);
+	if (chdir(oldpwd->value) == 0)
 	{
-		pwd = ft_strdup(get_key("PWD")->value);
-		if (env_set(ft_strdup("OLDPWD"), pwd) == 2)
-			return (printf("[!] cd - Failed to change environment!\n"), 1);
-		if (env_set(ft_strdup("PWD"), old_pwd) == 2)
-			return (printf("[!] cd - Failed to change environment!\n"), 1);
+		pwd = get_key("PWD");
+		if (env_set("OLDPWD", pwd->value) == 2)
+			return (printf("minishell (cd_swap_old) - "
+				  "envset: Failed to set OLDPWD!\n"), 1);
+		if (env_set("PWD", tmp) == 2)
+			return (printf("minishell (cd_swap_old) - "
+				  "envset: Failed to set PWD!\n"), 1);
 		return (0);
 	}
-	printf("[!] - Failed to change directory!\n");
+	perror("minishell (cd_swap_old) - chdir");
 	return (1);
 }
 
@@ -67,12 +86,13 @@ static int	cd_dir(t_command *cmd)
 	if (chdir(cmd->argv[1]) == 0)
 	{
 		pwd = getcwd(NULL, 0);
+		if (!pwd)
+			return (perror("minishell (cd_dir) - getcwd"), 1);
 		if (cd_update_env(old_pwd, pwd) == 0)
-			return (0);
-		printf("[!] cd - Failed to change environment!\n");
+			return (free(old_pwd), 0);
 		return (free(old_pwd), free(pwd), 1);
 	}
-	printf("[!] - Failed to change directory!\n");
+	perror("minishell (cd_swap_old) - chdir");
 	return (free(old_pwd), 1);
 }
 

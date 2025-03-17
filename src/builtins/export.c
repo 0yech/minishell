523b938f11770d11da4d9e8f3ace95102a12cd71
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-17 14:53:25 +0100

    add export_print()

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,11 +6,36 @@
 /*   By: cheyo <cheyo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 15:28:50 by cheyo             #+#    #+#             */
-/*   Updated: 2025/03/17 12:04:25 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/17 14:52:58 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/**
+ * @brief Prints all exported variables to stdout formatted as "export <var>".
+ * @return 0 if all went well, -1 if an error occurred.
+ */
+int	export_print(void)
+{
+	int		i;
+	char	**env;
+
+	env = env_to_char(*env_get());
+	if (!env)
+		return (-1);
+	i = 0;
+	while (env[i])
+	{
+		if (write(STDOUT_FILENO, "export ", 8) == -1
+			|| write(STDOUT_FILENO, env[i], ft_strlen(env[i])) == -1
+			|| write(STDOUT_FILENO, "\n", 1) == -1)
+			return (perror("minishell (export_print) - write"),
+				free_array(env), 0);
+		i++;
+	}
+	return (free_array(env), 0);
+}
 
 /**
  * @return 0 if everything went well, -1 if an error occurred.
@@ -20,11 +45,7 @@ int	ft_export(char *str)
 	char	**slices;
 
 	if (!str)
-	{
-		// TODO: Need to write the exported values with export beforehand
-		ft_env();
-		return (0);
-	}
+		return (export_print());
 	if (ft_strchr(str, '='))
 	{
 		slices = ft_split(str, '=');

1bc4bb2d3706b87fc4327e27c9765e654c32e257
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-17 11:05:32 +0100

    echo builtin is done! removed TODO

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 15:04:48 by estettle          #+#    #+#             */
-/*   Updated: 2025/03/14 14:16:40 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/17 11:05:10 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ void	free_array(char **array)
 
 // TODO : Add non interactive mode
 // TODO : Use perror everywhere that's applicable ([!] - minishell (<function>) - <failed syscall function>)
-// TODO : echo builtin
 // TODO : add SHELL env var support
 // TODO : add ? env var support
 // TODO : Fix still reachable leaks

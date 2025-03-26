765ac9ba6ee99aa70176151765b62941bd6ba899
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-03-26 13:09:13 +0100

    parsing_args.c comments

--- src/parsing/parsing_args.c
+++ src/parsing/parsing_args.c
@@ -6,12 +6,20 @@
 /*   By: estettle <estettle@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 09:00:08 by estettle          #+#    #+#             */
-/*   Updated: 2025/03/17 12:00:24 by estettle         ###   ########.fr       */
+/*   Updated: 2025/03/26 13:08:25 by estettle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// TODO : make this return an int
+/**
+ * @brief Loops over a token list and a freshly created command list and fills
+ * the latter with information contained in the token.
+ *
+ * @param cmd A pointer to the first element of the command list.
+ * @param token A pointer to the first element of the token list.
+ */
 void	fill_args_fds(t_command *cmd, t_token *token)
 {
 	t_command	*current;
@@ -28,14 +36,14 @@ void	fill_args_fds(t_command *cmd, t_token *token)
 		else if (current && current->fdio)
 		{
 			if (token->type == REDIRECT_IN && token->next
-				&& token->next->type == REDIRECT_FILE) // <
+				&& token->next->type == REDIRECT_FILE)
 			{
 				current->fdio->input = ft_strdup(token->next->value);
 				if (!current->fdio->input)
 					perror("minishell (fill_args_fds) - ft_strdup");
 			}
 			else if (token->type == REDIRECT_OUT && token->next
-				&& token->next->type == REDIRECT_FILE) // >
+				&& token->next->type == REDIRECT_FILE)
 			{
 				current->fdio->outtype = RED_OUT;
 				current->fdio->output = ft_strdup(token->next->value);
@@ -43,10 +51,10 @@ void	fill_args_fds(t_command *cmd, t_token *token)
 					perror("minishell (fill_args_fds) - ft_strdup");
 			}
 			else if (token->type == APPEND && token->next
-				&& token->next->type == REDIRECT_FILE) // >>
+				&& token->next->type == REDIRECT_FILE)
 			{
 				current->fdio->outtype = APD;
-				current->fdio->output = ft_strdup(token->next->value); // TODO noticed this leaked (a while ago, might have been fixed)
+				current->fdio->output = ft_strdup(token->next->value);
 				if (!current->fdio->output)
 					perror("minishell (fill_args_fds) - ft_strdup");
 				current->fdio->fdout = O_WRONLY | O_CREAT | O_APPEND;
@@ -63,6 +71,10 @@ void	fill_args_fds(t_command *cmd, t_token *token)
 	}
 }
 
+/**
+* @brief Takes a token as argument and counts the number of options and
+* arguments contained within, then returns it.
+*/
 int	count_argsopt(t_token *token)
 {
 	int	count;
@@ -77,6 +89,12 @@ int	count_argsopt(t_token *token)
 	return (count);
 }
 
+/**
+ * @brief Extracts arguments and options from a given token.
+ *
+ * @param token The token to analyze.
+ * @return A double char pointer containing a string for each argument / option.
+ */
 char	**extract_args(t_token *token)
 {
 	int		count;

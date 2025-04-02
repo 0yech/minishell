155355dd85414e9c1576f4af8533dacf81cc331e
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-04-02 11:15:37 +0200

    updated finished todos

--- src/command_handler.c
+++ src/command_handler.c
@@ -17,22 +17,6 @@ void	command_handler(t_token **token_list)
 	t_command	*cmd;
 
 	cmd = parsing_handler(token_list);
-	if (!cmd)
-	{
-		// Used for valid redirections without commands, currently is being called
-		// even when a command node was printed, cause parser returned NULL node,
-		// but is checked on start. WARNING - multiple invalid commands will enter this,
-		// must yet be taken care of.
-		// no_cmd_checks(*token_list);
-	}
-	// TODO : Single invalid Heredoc should still create an fd when there's one. Only Check it when no command exists, pipe stuff should work fine already.
-	// TODO : Empty command list but not empty tokens. Should create fds when needed.
-	// TODO : Nvm, pipe multiple heredocs are not fine, they do not create a second command node.
-
-	// TODO : Additional error checks and "parser" for invalid redirections that still need fds
-
-	// TODO : Once a "custom" fd is set in a command, it will put it in regardless of
-	// the commands order. This probably was the reason fds were closed often before.
 
 	//printf("%s\n", (*token_list)->value);
 	//printf("Executable found : %s\n", find_executable_path(cmd->command));

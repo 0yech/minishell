77b5a7e009684aeaa91f4b16ff21a0ca5d046cbe
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-04-07 15:18:12 +0200

    finished error messages for invlid redirections and pipes

--- src/parsing/invalid_checks.c
+++ src/parsing/invalid_checks.c
@@ -6,7 +6,7 @@
 /*   By: fireinside <aisling.fontaine@protonmail    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 07:31:29 by nrey              #+#    #+#             */
-/*   Updated: 2025/04/07 13:48:55 by fireinside       ###   ########.fr       */
+/*   Updated: 2025/04/07 15:13:05 by fireinside       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,56 @@
 
 // TODO : "<< > o" OR "<< >" should print new error syntax
 
-int	is_token_after_pipe_invalid(t_token_type type)
+bool	is_token_after_pipe_invalid(t_token_type type)
 {
 	return (type == PIPE || type == REDIRECT_IN || type == REDIRECT_OUT
 			|| type == APPEND || type == HEREDOC);
+}
+
+int	is_valid_pipe(t_token *token)
+{
+	if (token->type == PIPE)
+	{
+		if (!token->next || is_token_after_pipe_invalid(token->next->type))
+		{
+			write(STDERR_FILENO, "syntax error near unexpected token `|'\n", 39);
+			return (-1);
+		}
+	}
+	return (0);
+}
+
+int	is_valid_redirection(t_token *token)
+{
+	if (token->type == REDIRECT_IN || token->type == REDIRECT_OUT
+		|| token->type == APPEND || token->type == HEREDOC)
+	{
+		if (!token->next)
+		{
+			write(STDERR_FILENO, "syntax error near unexpected token `newline'\n", 45);
+			return (-1);
+		}
+		else if ((token->type != HEREDOC && token->next->type != REDIRECT_FILE)
+			|| (token->type == HEREDOC && token->next->type != DELIM))
+		{
+			write(STDERR_FILENO, "syntax error near unexpected token `", 37);
+			write(STDERR_FILENO, token->next->value, ft_strlen(token->next->value));
+			write(STDERR_FILENO, "'\n", 2);
+			return (-1);
+		}
+	}
+	return (0);
 }
 
 int	valid_pipes(t_token *token)
 {
 	while (token)
 	{
-		if (token->type == PIPE)
+		if (is_valid_pipe(token) != 0 || is_valid_redirection(token) != 0)
 		{
-			if (!token->next || is_token_after_pipe_invalid(token->next->type))
-				write(STDERR_FILENO, "syntax error near unexpected token '|'\n", 39);
-			else
-			{
-				token = token->next;
-				continue ;
-			}
 			env_set_key("?", "2");
 			return (1);
 		}
-		if ((token->type == REDIRECT_IN || token->type == REDIRECT_OUT
-			|| token->type == APPEND || token->type == HEREDOC)
-		   && (!token->next || token->next->type != REDIRECT_FILE))
-	   	{	
-		   write(STDERR_FILENO, "syntax error near unexpected token 'newline'\n", 46);
-		   env_set_key("?", "2");
-		   return (1);
-	   	}
 		token = token->next;
 	}
 	return (0);

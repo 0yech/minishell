9f108bec124e0861585d610e8cbe27980a1e6090
nrey <Nelson.rey02@gmail.com>
2025-04-07 18:00:11 +0200

    new git handler

--- src/gitprompt/git_handler.c
+++ src/gitprompt/git_handler.c
@@ -6,7 +6,7 @@
 /*   By: nrey <nrey@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 04:57:02 by nrey              #+#    #+#             */
-/*   Updated: 2025/03/14 13:48:13 by estettle         ###   ########.fr       */
+/*   Updated: 2025/04/07 17:59:39 by nrey             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,80 @@ void	git_str_replace(char branch[128], size_t bytes_read)
 	}
 }
 
+char	*cut_pwd(char *path)
+{
+	int		i;
+	char	*p;
+
+	p = NULL;
+	if (!path)
+		return (NULL);
+	i = ft_strlen(path) - 1;
+	if (i <= 0)
+		return (NULL);
+	if (ft_strchr(path, '/'))
+	{
+		while (i > 0 && path[i] != '/')
+			i--;
+		if (path[i] == '/')
+		{
+			p = malloc(i + 1);
+			if (!p)
+				return (NULL);
+			ft_strlcpy(p, path, i + 1);
+			return (p);
+		}
+	}
+	return (NULL);
+}
+
+static int	try_open_head(char *dir)
+{
+	char	*head_path;
+	int		fd;
+
+	head_path = ft_strjoin(dir, "/.git/HEAD");
+	if (!head_path)
+		return (-1);
+	fd = open(head_path, O_RDONLY);
+	free(head_path);
+	return (fd);
+}
+
+static int	is_folder_root(char *path)
+{
+	return (ft_strlen(path) == 1 && ft_strncmp(path, "/", 1) == 0);
+}
+
+int	find_git_head(void)
+{
+	t_env	*pwd;
+	char	*dir;
+	int		fd;
+	char	*new_dir;
+
+	pwd = env_get_key("PWD");
+	if (!pwd || !pwd->value)
+		return (-1);
+	dir = ft_strdup(pwd->value);
+	if (!dir)
+		return (-1);
+	while (1)
+	{
+		fd = try_open_head(dir);
+		if (fd >= 0)
+			return (free(dir), fd);
+		if (is_folder_root(dir))
+			break ;
+		new_dir = cut_pwd(dir);
+		free(dir);
+		if (!new_dir)
+			return (-1);
+		dir = new_dir;
+	}
+	return (-1);
+}
+
 // TODO : Recursive git prompt
 char	*get_git_branch(void)
 {
@@ -32,7 +106,7 @@ char	*get_git_branch(void)
 	char	*branch_name;
 	ssize_t	bytes_read;
 
-	fd = open(".git/HEAD", O_RDONLY);
+	fd = find_git_head();
 	if (fd < 0)
 		return (NULL);
 	bytes_read = read(fd, branch, sizeof(branch) - 1);
